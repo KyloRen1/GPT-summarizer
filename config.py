72a0c4e6adf2e8bb45c25b339abe899fd953eafa
KyloRen1 <ivanyuk.bogdan1999@gmail.com>
2023-03-26 19:52:44 +0200

    added util files

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 from pathlib import Path
 
 OPENAI_KEY_API = None  # enter yor OPENAI key here
-MAX_TOKENS = 3000
+MODEL_MAX_TOKENS = 3000 # maximum model input (minus 1000 for generation)
+TOKE_WORD_COEF = 0.75  # on evarage 100 tokens is ±75 words.
 
 # Directories
 BASE_DIR = Path(__file__).parent.absolute()
--- main.py
+++ main.py
@@ -16,8 +16,7 @@ def parse_args():
     parser.add_argument("--filepath", required=True)
     parser.add_argument("--model_engine", default="gpt-3.5-turbo")
     parser.add_argument(
-        "--max_tokens", default=300, 
-        help="maximum number of tokens for summarization"
+        "--max_tokens", default=300, help="maximum number of tokens for summarization"
     )
     return parser.parse_args()
 
@@ -43,11 +42,11 @@ def open_ai_send_request(text_to_summarize: str, engine: str, max_tokens: int) -
 
 
 def request_summary(text_to_summarize: str, args) -> str:
-    # on evarage 100 tokens is ±75 words.
-    if len(text_to_summarize) * config.TOKE_WORD_COEF > config.MAX_TOKENS:
+    # on evarage 100 tokens is ±75 words
+    if len(text_to_summarize) * config.TOKE_WORD_COEF > config.MODEL_MAX_TOKENS:
         summary = list()
-        for i in trange(0, len(text_to_summarize), config.MAX_TOKENS):
-            chunk = " ".join(text_to_summarize[i : i + config.MAX_TOKENS])
+        for i in trange(0, len(text_to_summarize), config.MODEL_MAX_TOKENS):
+            chunk = " ".join(text_to_summarize[i : i + config.MODEL_MAX_TOKENS])
             chunck_summary = open_ai_send_request(
                 chunk, args.model_engine, args.max_tokens
             )
@@ -66,8 +65,9 @@ def summarize(args) -> None:
     filename = config.CACHE_DIR / parse_filename(args.filepath)
     save_txt(filename, text)
 
-    print("Estimated number of tokens in text: ", 
-          int(len(text) * config.TOKE_WORD_COEF))
+    print(
+        "Estimated number of tokens in text: ", int(len(text) * config.TOKE_WORD_COEF)
+    )
     # send for summarization
     # summary = request_summary(text, args)
     # save_txt(str(filename) + f'_summary_{args.max_tokens}_tokens', summary)
